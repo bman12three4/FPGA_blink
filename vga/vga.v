ce76cae512d76b9d5b81d992df8f0b3740b3bb7a
Byron Lathi <bslathi19@gmail.com>
2019-12-26 16:34:24 -0500

    Multicolor mode

--- vga/vga.v
+++ vga/vga.v
@@ -1,52 +1,84 @@
-module vga (input clk, output reg [3:0] vgadat, output reg hsync, output reg vsync);
+module vga (
+	input clk, 
+	output reg [3:0] redvga,
+	output reg [3:0] grnvga,
+	output reg [3:0] bluvga,
+	output reg hsync, 
+	output reg vsync
+	);
 	
-	reg [31:0] counter;
-	reg [31:0] vgatimer;
+	reg [14:0] counter;
+	reg [10:0] vgatimer;
 	reg [9:0] line;
+	reg [3:0] grad;
+	reg [2:0] color;
 	
-	reg [3:0] redvga;
+	reg [3:0] tmpredvga;
+	reg [3:0] tmpgrnvga;
+	reg [3:0] tmpbluvga;
 	
 	always @ (posedge clk)
 	begin
-		if (counter <= 5000000)
+		if (line == 0 && vgatimer == 0)
 		begin
 			counter <= counter + 1;
 		end
-		else
+		if (counter >= 5)
 		begin
-			counter  <= 0;
-			if (redvga < 15)
+			counter <= 0;
+			if (grad < 15)
 			begin
-				redvga <= redvga + 1;
+				grad = grad + 1;
+				tmpredvga <= tmpredvga + color[0];
+				tmpgrnvga <= tmpgrnvga + color[1];
+				tmpbluvga <= tmpbluvga + color[2];
 			end
 			else
 			begin
-				redvga <= 0;
+				grad <= 0;
+				tmpredvga <= 0;
+				tmpgrnvga <= 0;
+				tmpbluvga <= 0;
+				if (color < 7)
+				begin
+					color <= color + 1;
+				end
+				else
+				begin
+					color <= 0;
+				end
 			end
 		end
 		
 		if (vgatimer <= 190)
 		begin
 			hsync <= 0;
-			vgadat <= 0;
+			redvga <= 0;
+			grnvga <= 0;
+			bluvga <= 0;
 			vgatimer <= vgatimer + 1;
 		end
 		else if (vgatimer <= 285)
 		begin
 			hsync <= 1;
-			vgadat <= 0;
+			redvga <= 0;
+			grnvga <= 0;
+			bluvga <= 0;
 			vgatimer <= vgatimer + 1;
 		end
 		else if (vgatimer <= 1555)
 		begin
 			hsync <= 1;
-			vgadat <= redvga;
+			redvga <= tmpredvga;
+			grnvga <= tmpgrnvga;
+			bluvga <= tmpbluvga;
 			vgatimer <= vgatimer + 1;
 		end
 		else if (vgatimer <= 1585)
 		begin
 			hsync <= 1;
-			vgadat <= 0;
+			redvga <= 0;
+
 			vgatimer <= vgatimer + 1;
 		end
 		else 
